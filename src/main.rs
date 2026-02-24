0c1386ac60d07b5643d9209477524fe5ef289f31
Calum Price - Work <calumprice.dev@gmail.com>
2026-02-24 09:24:21 +0000

    added config.toml to set a config of the programs to launch

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
+use std::fs;
+use serde::Deserialize;
 use std::process::Command;
 
+#[derive(Debug, Deserialize)]
+struct App {
+    prog: String,
+    cmd: String,
+    args: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    app: Vec<App>,
+}
 fn main() {
-    let program = "Wezterm";
-    Command::new("open").args(["-a", program]).spawn().unwrap();
+    let config_file = fs::read_to_string("config.toml").unwrap();
+    let config: Config = toml::from_str(&config_file).unwrap();
+    for app in config.app {
+        Command::new(app.cmd).args([app.args, app.prog]).spawn().unwrap();
+    }
 }
