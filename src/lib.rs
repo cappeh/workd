49330fe806830166f2b853f4f0f7085e7c5c3f94
Calum Price - Work <calumprice.dev@gmail.com>
2026-02-24 11:13:51 +0000

    added spawn apps function

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::{fs, io};
+use std::process::Command;
 use serde::Deserialize;
-use toml::de::Error;
+use toml::de;
+use std::error::Error;
 
 #[derive(Debug, Deserialize)]
 pub struct App {
@@ -18,7 +20,18 @@ pub fn load_config_toml_file() -> Result<String, io::Error> {
     fs::read_to_string("config.toml")
 }
 
-pub fn read_toml_to_config(file: String) -> Result<Config, Error> {
+pub fn read_toml_to_config(file: String) -> Result<Config, de::Error> {
    let config = toml::from_str(&file)?;
    Ok(config)
+}
+
+pub fn spawn_apps() -> Result<(), Box<dyn Error>>{
+    let config_file = load_config_toml_file()?;
+    let config = read_toml_to_config(config_file)?;
+
+    for app in config.app {
+        Command::new(app.cmd).args([app.args, app.prog]).spawn()?;
+    }
+
+    Ok(())
 }
