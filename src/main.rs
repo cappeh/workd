c10869abab4997f1b7f7a91e6a5de763b4f13c24
Calum Price - Work <calumprice.dev@gmail.com>
2026-02-24 11:16:08 +0000

    added logic to main to run the application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::process;
+use workd::spawn_apps;
+
 fn main() {
-    println!("Workd");
+    if let Err(e) = spawn_apps() {
+        eprintln!("Error starting applications: {e}");
+        process::exit(1);
+    }
 }
