9aa6abb8fd7db83468cc503a8960f1bf77f98333
Calum Price - Work <calumprice.dev@gmail.com>
2026-02-24 08:11:13 +0000

    explore other options of calling Command::new args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::process::Command;
 
 fn main() {
-    Command::new("open").arg("-a").arg("Wezterm").spawn().unwrap();
+    let program = "Wezterm";
+    Command::new("open").args(["-a", program]).spawn().unwrap();
 }
